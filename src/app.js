0bd3bd566bb8c678a9e1d5caa27b6ebf2d721857
joanlitaponte <joanlitaponte@gmail.com>
2026-04-15 21:14:33 +0000

    cartas de poker

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let palos = ["♦", "♥", "♠", "♣"];
+  let numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  let palo = palos[Math.floor(Math.random() * palos.length)];
+  let numero = numeros[Math.floor(Math.random() * numeros.length)];
+
+  document.getElementById("palo-arriba").innerHTML = palo;
+  document.getElementById("palo-abajo").innerHTML = palo;
+  document.getElementById("numero").innerHTML = numero;
+
+  let carta = document.getElementById("carta");
+
+  if (palo === "♥") {
+    carta.className = "card heart";
+  } else if (palo === "♦") {
+    carta.className = "card diamond";
+  } else if (palo === "♠") {
+    carta.className = "card spade";
+  } else {
+    carta.className = "card club";
+  }
+
 };
